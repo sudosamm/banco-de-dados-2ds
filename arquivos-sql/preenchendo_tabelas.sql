e4865ae4652f3169d67810a0bad3362b3c97ee2b
Samuel <alpacadechinelo@gmail.com>
2026-03-04 16:42:46 -0300

    Create preenchendo_tabelas.sql

---
+++ arquivos-sql/preenchendo_tabelas.sql
@@ -0,0 +1,12 @@
+INSERT INTO clientes (nome, email, cpf, idade) VALUES
+('Mateus', 'jmateus@yahoo.net','12345678985', '67'),
+('Louis', 'batmandecropped@orkut.msn', '65168541234','69'),
+('Manel', 'oloko1234@hotmail.br', '40028922', '-1');
+
+INSERT INTO categorias (nome) VALUES
+('Laticínios'), ('Frios'), ('Bebidas'), ('Rações'),
+('Ultraprocessados'), ('Frutas'), ('Verduras'), ('Legumes'),
+('Instantâneos');
+
+INSERT INTO comidas (nome, preco, estoque, categoria_id) VALUES
+('Leite Integral 1L', '7.49', '200', '1');
